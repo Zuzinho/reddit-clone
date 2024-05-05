29ef576155bc3b6b5573ce727576ef59ef23c279
Никита Зозуля <nik.zozulya.04@mail.ru>
2024-05-05 19:19:18 +0300

    Created id generator

---
+++ pkg/id/id.go
@@ -0,0 +1,23 @@
+package id
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+const length = 24
+
+func GenerateID() string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
